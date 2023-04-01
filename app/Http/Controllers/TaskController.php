2002400704e44795e6984a41f554660dd04b408e
Mario Gomes <markkennedy232323@gmail.com>
2023-04-01 17:22:53 +0000

    Add Migration

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    //
+    public function index()
+    {
+
+    }
+    public function index()
+    {
+
+    }
+    public function index()
+    {
+
+    }
+    public function index()
+    {
+
+    }
+    public function index()
+    {
+
+    }
+}
